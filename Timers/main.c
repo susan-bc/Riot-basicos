9506b8a7d3eb9237858b420ba2896e3f4ee3f044
susan-bc <susan.silva@icen.ufpa.br>
2025-05-20 19:32:59 -0300

    Update main.c

--- Timers/main.c
+++ Timers/main.c
@@ -9,24 +9,24 @@
 static char stack[THREAD_STACKSIZE_MAIN];
 
 static void *thread_handler(void *arg){
-  int counter = 0;
-  while(1){
-    counter++;
-    printf("Message %d from %s\n", counter, (char *)arg);
-    ztimer_sleep(ZTIMER_MSEC, 1000);
-  }
-  return NULL;
+	int counter = 0;
+	while(1){
+		counter++;
+		printf("Message %d from %s\n", counter, (char *)arg);
+		ztimer_sleep(ZTIMER_MSEC, 1000);
+	}
+	return NULL;
 }
 
 int main(void){
-  thread_create(stack, sizeof(stack), THREAD_PRIORITY_MAIN -1, 0, thread_handler, "my thread", "thread");
+	thread_create(stack, sizeof(stack), THREAD_PRIORITY_MAIN -1, 0, thread_handler, "my thread", "thread");
 	int counter = 0;
   
 	while(1){
-	  counter++;
-	  printf("Message %d from main thread\n", counter);
-	  ztimer_sleep(ZTIMER_MSEC, 500);
+		counter++;
+		printf("Message %d from main thread\n", counter);
+		ztimer_sleep(ZTIMER_MSEC, 500);
 	}
-
-  return 0;
+	
+	return 0;
 }
